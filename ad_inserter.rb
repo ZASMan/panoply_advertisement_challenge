17db92848084f2dfd563dd92e4e59017435d5aa3
Zane Sandin <i.zane.am.robot@gmail.com>
2018-01-29 21:04:26 -0500

    set up some example data

--- ad_inserter.rb
+++ ad_inserter.rb
@@ -33,3 +33,47 @@ class AdInserter
 
   end
 end
+
+# Example Episodes
+
+example_episode_1 = { audio: "[PRE]+++++[MID]+++++", id: "dag-532" }
+example_episode_2 = { audio: "+++++[MID]++++[MID]++++[POST]", id: "kld-412" }
+example_episode_3 = { audio: "[PRE][PRE]+++++[MID]+++[MID]+++++[POST]", id: "abc-444" }
+
+# Example Ad Campaigns
+
+example_ad_campaign_1 = [
+{ audio: "*AcmeA*", type: "PRE", targets: ["dag-523"], revenue: 12 },
+{ audio: "*AcmeB*", type: "MID", targets: ["dag-523"], revenue: 4 },
+{ audio: "*AcmeC*", type: "MID", targets: ["dag-523"], revenue: 14 }
+]
+
+example_ad_campaign_2 = [
+{ audio: "*CorpCorp*", type: "POST", targets: ["afs-354", "dag-523"], revenue: 3 }
+]
+
+# Currently Running Ad Campaigns
+
+ad_campaign_1 = [
+{ audio: "*AcmeA*", type: "PRE", targets: ["dag-892", "hab-812"], revenue: 1 },
+{ audio: "*AcmeB*", type: "MID", targets: ["dag-892", "hab-812"], revenue: 4 },
+{ audio: "*AcmeC*", type: "MID", targets: ["dag-892", "hab-812"], revenue: 5 }
+]
+
+ad_campaign_2 = [
+{ audio: "*TacoCat*", type: "MID", targets: ["abc-123", "dag-892"], revenue: 3 }
+]
+
+ad_campaign_3 = [
+{ audio: "*CorpCorpA*", type: "PRE", targets: ["abc-123", "dag-892"], revenue: 11 },
+{ audio: "*CorpCorpB*", type: "POST", targets: ["abc-123", "dag-892"], revenue: 7 },
+]
+ad_campaign_4 = [
+{ audio: "*FurryDogA*", type: "PRE", targets: ["dag-892", "hab-812", "efa-931"], revenue: 11 },
+{ audio: "*FurryDogB*", type: "PRE", targets: ["dag-892", "hab-812", "efa-931"], revenue: 7 },
+]
+
+ad_campaign_5 = [
+{ audio: "*GiantGiraffeA*", type: "MID", targets: ["paj-103", "abc-123"], revenue: 9 },
+{ audio: "*GiantGiraffeB*", type: "MID", targets: ["paj-103", "abc-123"], revenue: 4 },
+]
